8a0cedbfab8b658efaee720022a9284d92ecf0f0
Hasan Kumar <769950+mintuhouse@users.noreply.github.com>
2026-08-30 00:36:26 +0530

    Use Vite development mode for dev utilities

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -27,12 +27,8 @@ const legalAssets = [
     })),
 ]
 
-const viteConfig = defineConfig(({ command, isPreview, mode }) => {
+const viteConfig = defineConfig(({ mode }) => {
   return {
-    define: {
-      // `import.meta.env.DEV` can be true for `NODE_ENV=development vite build`. https://vite.dev/guide/env-and-mode.html#node-env-and-modes
-      __DEV_UTILITIES__: JSON.stringify(command === "serve" && !isPreview),
-    },
     resolve: { tsconfigPaths: true },
     build: {
       target: "es2025",
